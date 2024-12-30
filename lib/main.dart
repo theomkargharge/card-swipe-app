281d03b3aef1e917d3c2420325d9d3bc9b9ecf25
theomkargharge <omkarghargeOG@Gmail.com>
2024-12-30 12:38:25 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/route_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'card_swiper/binding/card_swiper_binding.dart';
 
 void main() {
